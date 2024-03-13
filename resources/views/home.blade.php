6fbc6b920baf4a9017419e80d3c919befbb48670
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-13 23:16:51 +0100

    added card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
 @extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <main>
+        <div class="container">
+            <div class="row py-4">
+                <div class="col-3">
+                    <div class="card">
+                        <div class="card-body">
+                          <h5 class="card-title">Card title</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <a href="#" class="card-link">Card link</a>
+                          <a href="#" class="card-link">Another link</a>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
