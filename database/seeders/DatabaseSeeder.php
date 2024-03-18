4af2f3d93d419ee7656adf8e703cb85aec1a0ca5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-18 01:01:23 +0100

    added factory into database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Train::factory(20)->create();
     }
 }
