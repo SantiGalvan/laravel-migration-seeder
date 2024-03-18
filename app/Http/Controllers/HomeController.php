948baf86dedf93e80620879ddc73c6eee52d93bd
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-18 01:25:41 +0100

    added trains

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('home');
+
+        $trains = Train::all();
+
+        return view('home', compact('trains'));
     }
 }
